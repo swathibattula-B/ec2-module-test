c25e6a759d2a0b863cbc4d9b517ab9985b44634b
swathi <battulaswathi15407@gmail.com>
2026-03-05 21:01:50 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "sg_ids" {
+    default = ["sg-0c4044beadcab573c"]
+}
+
+variable "component" {
+    default = "catalogue"
+}
