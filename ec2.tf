74b003be7071ac614534707fdec3f7ffb215bf40
swathi <battulaswathi15407@gmail.com>
2026-04-03 22:50:12 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,13 @@
 module "ec2" {
     source = "../terraform-aws-instance"
+    #project = "roboshop"
+    project = var.project_name
+    environment = var.environment
+    ami_id = data.aws_ami.joindevops.id
+    sg_ids = var.sg_ids
+    instance_type = "t3.large"
+    tags = {
+        Name = "${var.project_name}-${var.env}-${var.component}"
+        Component = var.component
+    }
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "sg_id" {
+    default = ["sg-0c4044beadcab573c"]
+}
+
+variable "component" {
+    default = "catalogue"
+}
